61ffd257b321bbf9ec5e1d23e886764c714e3621
Robert Brons <robbrons35@gmail.com>
2023-12-10 19:51:28 +0600

    Added login success handler for role based redirects

--- src/main/java/Halms/Watson/configuration/WebSecurityConfig.java
+++ src/main/java/Halms/Watson/configuration/WebSecurityConfig.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -43,6 +45,7 @@ public class WebSecurityConfig {
                         .loginPage("/login")
                         .permitAll()
                         .failureUrl("/login-error")
+                        .successHandler(new CustomSuccesHandler())
                 )
                 .logout(logout -> {
                     logout.logoutSuccessUrl("/").permitAll();
